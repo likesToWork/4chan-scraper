b480c7e3f15c5481f3de6fdbb59033192afa998e
likesToWork <hellofunnywerk@proton.me>
2024-12-11 17:02:53 -0800

    Update 4chan Downloader.py
    It actually works now...

--- 4chan Downloader.py
+++ 4chan Downloader.py
@@ -1,138 +1,98 @@
-import urllib, json, os, datetime
+import urllib.request
+import json
+import os
+import datetime
 from unidecode import unidecode
-import unicodecsv as csv
+import csv
 
-
-
-#CHANGE THESE SETTINGS!
+# CHANGE THESE SETTINGS!
 saveDirectory = '/path/to/parent/directory/'
 boardLetter = 'pol'
 
-print ("The current working directory is " + saveDirectory)  
+print(f"The current working directory is {saveDirectory}")
 
-#First we'll try to create a folder for the board.
-path = saveDirectory + boardLetter
-print ("Attempting to create directory for board at %s" % path)
-try:  
-	os.mkdir(path)
-except OSError:  
-	print ("Creation of the directory %s failed  - possible directory already exists" % path)
-else:  
-	print ("Successfully created the directory %s" % path)
+# Create a folder for the board if it doesn't exist
+path = os.path.join(saveDirectory, boardLetter)
+print(f"Attempting to create directory for board at {path}")
+os.makedirs(path, exist_ok=True)
+print(f"Directory for board {path} {'already exists' if os.path.exists(path) else 'created successfully'}")
 
-#Get the 4chan board catalog JSON file and open it
-url = "https://a.4cdn.org/" + boardLetter + "/catalog.json"
-response = urllib.urlopen(url)
+# Get the 4chan board catalog JSON file and open it
+url = f"https://a.4cdn.org/{boardLetter}/catalog.json"
+response = urllib.request.urlopen(url)
 threadCatalog = json.loads(response.read())
 
-#LOL I HAVE NO IDEA WHAT I'M DOING!
-
 print("BEGINNING 4CHAN FRONT PAGE SCRAPE")
-print("Current board: " + boardLetter)
+print(f"Current board: {boardLetter}")
 
 downloadCounter = 0
 
-#Only look at the front page
+# Only look at the front page
 frontPage = threadCatalog[0]['threads']
-#print frontPage[0]['com']
 
-#Create a file to list all threads we've analyzed
-allThreadFile = saveDirectory + boardLetter + "/frontPage-" + str(datetime.datetime.now()) + ".csv"
-with open(allThreadFile, mode='a') as thread_file:
-			thread_writer = csv.writer(thread_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-			thread_writer.writerow(["timeStamp", "lastScraped", "posterID", "name", "postID", "subjectText", "commentText"])
+# Create a file to list all threads we've analyzed
+allThreadFile = os.path.join(path, f"frontPage-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.csv")
+with open(allThreadFile, mode='w', newline='', encoding='utf-8') as thread_file:
+    thread_writer = csv.writer(thread_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    thread_writer.writerow(["timeStamp", "lastScraped", "posterID", "name", "postID", "subjectText", "commentText"])
 
-print ("Now parsing front page threads...")
-#There are always 20 threads on the front page. So we'll loop through and read each one
-i = 0
+print("Now parsing front page threads...")
+# There are always 20 threads on the front page. So we'll loop through and read each one
+for i, thread in enumerate(frontPage[:20]):
+    threadNumber = thread['no']
+    print(f"STARTING NEW THREAD - #{threadNumber}")
+    
+    subjectText = thread.get('sub', "No Subject Text Provided")
+    commentText = thread.get('com', "No Comment Text Provided")
+    posterID = thread.get('id', "No ID")
+    name = thread.get('name', "Anonymous")
+    
+    with open(allThreadFile, mode='a', newline='', encoding='utf-8') as thread_file:
+        thread_writer = csv.writer(thread_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        thread_writer.writerow([thread['now'], datetime.datetime.now(), posterID, name, threadNumber, subjectText, commentText])
+    
+    # Create a new directory for the thread
+    threadPath = os.path.join(path, f"{threadNumber} - {thread['semantic_url']}")
+    print(f"Attempting to create directory for thread {threadPath}")
+    os.makedirs(threadPath, exist_ok=True)
+    print(f"Directory for thread {threadPath} {'already exists' if os.path.exists(threadPath) else 'created successfully'}")
+    
+    # Create a CSV file for the individual thread
+    with open(os.path.join(threadPath, "thread.csv"), mode='w', newline='', encoding='utf-8') as thread_file:
+        thread_writer = csv.writer(thread_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        thread_writer.writerow(["timeStamp", "posterID", "name", "postID", "subjectText", "commentText", "filename"])
+    
+    # Get the individual thread JSON file from 4chan and open it
+    threadUrl = f"https://a.4cdn.org/{boardLetter}/thread/{threadNumber}.json"
+    response = urllib.request.urlopen(threadUrl)
+    individualThread = json.loads(response.read())
+    
+    # Loop through every post in this thread
+    for post in individualThread['posts']:
+        timeStamp = post.get('now', "")
+        name = post.get('name', "Anonymous")
+        posterID = post.get('id', "No ID")
+        postID = post.get('no', "")
+        subjectText = post.get('sub', "No Subject Text Provided")
+        commentText = post.get('com', "No Comment Text Provided")
+        
+        filename = "No File Posted"
+        if 'tim' in post:
+            OGfilename = unidecode(post.get('filename', ""))
+            renamedFile = str(post['tim'])
+            fileExtension = str(post['ext'])
+            filename = f"{OGfilename} - {renamedFile}{fileExtension}"
+            try:
+                postFileUrl = f"https://i.4cdn.org/{boardLetter}/{renamedFile}{fileExtension}"
+                postFilePath = os.path.join(threadPath, filename)
+                urllib.request.urlretrieve(postFileUrl, postFilePath)
+                downloadCounter += 1
+            except Exception as e:
+                print(f"File Download Error: {e}")
+                filename += " - Download Error"
+        
+        with open(os.path.join(threadPath, "thread.csv"), mode='a', newline='', encoding='utf-8') as thread_file:
+            thread_writer = csv.writer(thread_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+            thread_writer.writerow([timeStamp, posterID, name, postID, subjectText, commentText, filename])
 
-while i < 20:
-	print ("STARTING NEW THREAD - #" + str(frontPage[i]['no']))
-	
-	#Open the thread listing file, and write the information for the current thread
-	#First we make some failsafes - if not all the information is filled in.
-	if 'sub' in frontPage[i]:
-		subjectText = frontPage[i]['sub']
-	else:
-		subjectText = "No Subject Text Provided"
-	if 'com' in frontPage[i]:
-		commentText = frontPage[i]['com']
-	else:
-		commentText = "No Comment Text Provided"
-	with open(allThreadFile, mode='a') as thread_file:
-			thread_writer = csv.writer(thread_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-			try:
-				thread_writer.writerow([frontPage[i]['now'], datetime.datetime.now(), frontPage[i]['id'], frontPage[i]['name'], frontPage[i]['no'], subjectText, commentText])
-			except:
-				thread_writer.writerow([frontPage[i]['now'], datetime.datetime.now(), "No ID", frontPage[i]['name'], frontPage[i]['no'], subjectText, commentText])
-
-	
-	# First we create a new directory for the thread
-	path = saveDirectory + boardLetter + "/" + str(frontPage[i]['no']) + " - " + frontPage[i]['semantic_url']
-	print ("Attempting to create directory for thread %s" % path)
-	try:  
-	    os.mkdir(path)
-	except OSError:  
-	    print ("Creation of the directory %s failed  - possible directory already exists" % path)
-	else:  
-	    print ("Successfully created the directory %s" % path)
-
-	#Now we create a CSV File for the individual thread
-	with open(path + "/thread.csv", mode='w') as thread_file:
-			thread_writer = csv.writer(thread_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-			thread_writer.writerow(["timeStamp", "posterID", "name", "postID", "subjectText", "commentText", "filename"])
-
-	#Get the individual thread JSON file from 4chan and open it
-	url = "https://a.4cdn.org/" + boardLetter + "/thread/" + str(frontPage[i]['no']) + ".json"
-	response = urllib.urlopen(url)
-	individualThread = json.loads(response.read())
-	
-	#print individualThread['posts']
-	
-	#Next, we loop through every post in this thread, and start gathering information. I use j to count which post (within the thread) I'm on
-	j = 0
-	for post in individualThread['posts']:
-		#print post
-		print ("Now processing post " + str(individualThread['posts'][j]['no']))
-		timeStamp = individualThread['posts'][j]['now']
-		name = individualThread['posts'][j]['name']
-		try:
-			posterID = individualThread['posts'][j]['id']
-		except:
-			posterID = "No ID"
-		postID = individualThread['posts'][j]['no']
-		if 'sub' in individualThread['posts'][j]:
-			subjectText = individualThread['posts'][j]['sub']
-		else:
-			subjectText = "No Subject Text Provided"
-		if 'com' in individualThread['posts'][j]:
-			commentText = individualThread['posts'][j]['com']
-		else:
-			commentText = "No Comment Text Provided"
-		#Was there an image posted? Let's snag it!
-		if 'tim' in individualThread['posts'][j]:
-			OGfilename = unidecode(individualThread['posts'][j]['filename'])
-			#OGfilename = unidecode(OGfilename)
-			renamedFile = str(individualThread['posts'][j]['tim'])
-			fileExtension = str(individualThread['posts'][j]['ext'])
-			filename = OGfilename + " - " + renamedFile + fileExtension
-			postFile = urllib.URLopener()
-			try:
-				postFile.retrieve("https://i.4cdn.org/" + boardLetter + "/" + renamedFile + fileExtension, saveDirectory + "/" + boardLetter + "/" + str(frontPage[i]['no']) + " - " + frontPage[i]['semantic_url'] + "/" + filename)
-				downloadCounter = downloadCounter + 1
-			except:
-				print("Fille Download Error :(")
-				filename = filename + " - Download Error"
-		else:
-			filename = "No File Posted"
-		
-		with open(path + "/thread.csv", mode='a') as thread_file:
-			thread_writer = csv.writer(thread_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-			thread_writer.writerow([timeStamp, posterID, name, postID, subjectText, commentText, filename])
-	
-		j = j + 1
-
-	
-	#Iterate the loop
-	i = i + 1
-print ("Front Page Scrape Completed - " + str(downloadCounter) + " files downloaded")
+print(f"Front Page Scrape Completed - {downloadCounter} files downloaded")
